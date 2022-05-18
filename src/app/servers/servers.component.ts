818c81edbb31ed6a799e2f8e916b473960db94cc
tim <robbittim@gmail.com>
2022-05-18 18:09:24 -0500

    data binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,31 +4,30 @@ import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
-
   allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'prePopulated name';
 
   constructor(
-    private router:Router,
-    
+    private router: Router,
+
     /*
     inject the currently active routes
     this will be the route which loaded this component 
     */
-    private activeRoute:ActivatedRoute
-  ) { 
+    private activeRoute: ActivatedRoute
+  ) {
     setTimeout(() => {
       this.allowNewServer = true;
-    },2000)
+    }, 2000);
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  onReload(){
-    
+  onReload() {
     /*
     relative path with relative activeRoute 
     (current route: localhost:4200/servers)
@@ -36,7 +35,16 @@ export class ServersComponent implements OnInit {
     navigate to: localhost:4200/servers/{relativePath}
  
     */
-    this.router.navigate(['servers'],{relativeTo:this.activeRoute});
+    this.router.navigate(['servers'], { relativeTo: this.activeRoute });
   }
 
+  onCreateServer() {
+    this.serverCreationStatus = 'Server created!!! name is ' + this.serverName;
+  }
+
+  onUpdateServeName(event: Event) {
+    console.log(event);
+
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
